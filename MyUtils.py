dba44c5d23441873b35471b99e05cf802ee21fb9
mukund-rajukumar <139948136+mukund-rajukumar@users.noreply.github.com>
2024-09-27 13:23:56 -0700

    Delete MyUtils.py

--- MyUtils.py
+++
@@ -1,9 +0,0 @@
-def count_params(model):
-  total_params = 0
-  for param in model.parameters():
-    num_elements = 1
-    for dim in param.size():
-        num_elements *= dim
-    total_params += num_elements
-  print(f"{total_params / 1000000:.1f} million parameters")
-
